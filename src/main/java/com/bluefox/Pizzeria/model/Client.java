c41ea58641f1ce0694d84e59c372486ffaaf7719
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2025-05-17 22:47:24 -0300

    feat: created Person and Client class

---
+++ src/main/java/com/bluefox/Pizzeria/model/Client.java
@@ -0,0 +1,34 @@
+package com.bluefox.Pizzeria.model;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * The Client class represents a client, extending the Person class, with customer-specific attributes.
+ */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@SuperBuilder
+public class Client extends Person {
+
+    private LocalDate birthday;
+    @Builder.Default
+    private LocalDate dateLastPurchase = null;
+    @Builder.Default
+    private List<UUID> orders = new ArrayList<>();
+    @Builder.Default
+    private int loyaltyPoints = 0;
+    @Builder.Default
+    private boolean vip = false;
+    @Builder.Default
+    private String preferredPaymentMethod = "";
+    @Builder.Default
+    private String notes = "";
+}
---
+++ src/main/java/com/bluefox/Pizzeria/model/Person.java
@@ -0,0 +1,31 @@
+package com.bluefox.Pizzeria.model;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ * The Person class represents a person with common user attributes.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Person {
+
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String address;
+    @Builder.Default
+    private String role = "client";
+    @Builder.Default
+    private boolean active = true;
+    @Builder.Default
+    private LocalDate accountCreationDate = LocalDate.now();
+}
