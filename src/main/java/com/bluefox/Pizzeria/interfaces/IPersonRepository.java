2d096681d15adcccf3cdb2029735d2fd0d14138e
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-05-18 11:37:31 -0300

    feat: created IPersonRepository

---
+++ src/main/java/com/bluefox/Pizzeria/interfaces/IPersonRepository.java
@@ -0,0 +1,13 @@
+package com.bluefox.Pizzeria.interfaces;
+
+import com.bluefox.Pizzeria.model.people.Person;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public interface IPersonRepository extends IRepository<Person> {
+    Person findByName(String username) throws IllegalArgumentException, NoSuchElementException;
+    Person findByEmail(String email) throws IllegalArgumentException, NoSuchElementException;
+    Person findByPhoneNumber(String phoneNumber) throws IllegalArgumentException, NoSuchElementException;
+    List<Person> findByType(Class<?> clazz) throws IllegalArgumentException, NoSuchElementException;
+}
